0f9e472a18f8c1da72281bc33726625878c37bf1
BySajed <sajed.benyoussef@hotmail.com>
2025-12-10 17:25:37 -0500

    define struct

--- fat32_reader/src/main.rs
+++ fat32_reader/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{self, Read, Seek, SeekFrom};
+use std::path::Path;
+use byteorder::{LittleEndian, ReadBytesExt};
+
+#[derive(Debug, Clone)]
+pub struct BootSector {
+    pub bytes_per_sector: u16,
+    pub sectors_per_cluster: u8,
+    pub reserved_sector: u16,
+    pub number_of_fats: u8,
+    pub sectors_per_fat: u32,
+    pub root_dir_cluster: u32, 
+}
+
+pub struct Fat32Image {
+    file: File,
+    pub boot_sector: BootSector,
 }
