273050f251c5a82818c45352984339b7f5610ebc
BySajed <sajed.benyoussef@hotmail.com>
2025-12-10 17:17:52 -0500

    Initial commit

---
+++ fat32_reader/target/debug/deps/fat32_reader-3f581c66d028fdc5.d
@@ -0,0 +1,5 @@
+/home/kali/Documents/Rust/fat32_project/fat32_reader/target/debug/deps/libfat32_reader-3f581c66d028fdc5.rmeta: src/main.rs
+
+/home/kali/Documents/Rust/fat32_project/fat32_reader/target/debug/deps/fat32_reader-3f581c66d028fdc5.d: src/main.rs
+
+src/main.rs:
