2f6cfe1aaf6758f0556334ba13c506e1da90640b
BySajed <sajed.benyoussef@hotmail.com>
2025-12-10 17:32:42 -0500

    implementation FAT32

--- fat32_reader/src/main.rs
+++ fat32_reader/src/main.rs
@@ -17,3 +17,41 @@ pub struct Fat32Image {
     file: File,
     pub boot_sector: BootSector,
 }
+
+impl Fat32Image {
+    pub fn new<P: AsRef<Path>>(path: P) -> io::Result<Self> {
+        let mut file = File::open(path)?;
+
+        // 1. Byte per sector (offset 11, 2 bytes)
+        file.seek(SeekFrom::Start(11))?;
+        let bytes_per_sector = file.read_u16::<LittleEndian>()?;
+
+        // 2. Sectors per cluster (offset 13, 1 byte)
+        let sectors_per_cluster = file.read_u8()?;
+
+        // 3. Reserved sectors (offset 14, 2 bytes)
+        let reserved_sector = file.read_u16::<LittleEndian>()?;
+
+        // 4. Number of FATs (offset 16, 1 byte)
+        let number_of_fats = file.read_u8()?;
+
+        // 5. Sectors per FAT (offset 36, 4 bytes)
+        file.seek(SeekFrom::Start(36))?;
+        let sectors_per_fat = file.read_u32::<LittleEndian>()?;
+
+        // 6. Root directory cluster (offset 44, 4 bytes)
+        file.seek(SeekFrom::Start(44))?;
+        let root_dir_cluster = file.read_u32::<LittleEndian>()?;
+
+        let boot_sector = BootSector {
+            bytes_per_sector,
+            sectors_per_cluster,
+            reserved_sector,
+            number_of_fats,
+            sectors_per_fat,
+            root_dir_cluster,
+        };
+
+        Ok(Fat32Image { file, boot_sector })
+    }
+}
